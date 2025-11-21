97f554137f7ba6627b693779de9153edcac695c9
Grayson Moore <moor8456@hotmail.com>
2025-11-21 10:12:34 -0800

    Refactor calculation and clean up script.js
    Refactor calculation logic and remove password prompt.

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-/*
-var promp = prompt("Enter password");
-var pw = 1125;
-if (promp === pw) {
-	alert("You're in");
-} else {
-	while(promp !== pw) {
-		alert("Incorrect password");
-		promp = prompt("Enter password");
-	}
-}
-*/
-//calculation
-
 $("#button").click(function(event) {
-	var a = Number($("#inputh").val());
+	var hours = Number($("#inputh").val());
 	var b = Number($("#inputm").val());
-	var answer = a * 170 + b * 3.35;
+	var matcost = 0;
+	if (b < 20) {
+		matcost = b * 5;
+	} else if (b >= 20 && b <= 500) {
+		matcost = b * (13.9142 + (-3.9008 * Math.log(b)) + 0.3088 * (Math.log(b) ** 2));
+	} else if (b > 500) {
+		matcost = b * 1.6;
+	}
+	var answer = hours * 170 + matcost;
 	$("#answer h1").detach();
 	$("p").detach();
-	$("#answer").append("<p>Standard Price: </p><h1>$" + answer.toFixed(2) + "</h1>");
-	var answer2 = answer * 1.75;
+	$("#answer").append("<h1>$" + answer.toFixed(2) + "</h1>");
+	/*var answer2 = answer * 1.75;
 	$("#answer2 h1").detach();
-	$("#answer2").append("<p>After Hours Price: </p><h1>$" + answer2.toFixed(2) + "</h1>");
+	$("#answer2").append("<p>After Hours Price: </p><h1>$" + answer2.toFixed(2) + "</h1>");*/
 });
-
-
